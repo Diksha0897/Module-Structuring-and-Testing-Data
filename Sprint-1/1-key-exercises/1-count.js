f06558daa5e62dee786510dd797b87fe9e0f1b09
Diksha Aggarwal <dikshaaggarwal.in08@gmail.com>
2025-12-10 14:21:46 +0100

    describe the self assignment of count variable

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,5 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//Line 3 represents self assignment, where the variable count self assigns a new value based on its current value. For example, count was 0 before line 3, after executing line 3, count will be 0+1 i.e 1.//
