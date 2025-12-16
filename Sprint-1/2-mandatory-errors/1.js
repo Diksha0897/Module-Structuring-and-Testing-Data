09ddc06aeabf5c9745887b0158d1d333fc5e3e0b
Diksha Aggarwal <dikshaaggarwal.in08@gmail.com>
2025-12-16 21:16:04 +0100

    Refactor age variable declaration to use let instead of const and add explanatory comments

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,18 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
-age = age + 1;
+//const age = 33;
+//age = age + 1;
+
+// This code will cause an error because `age` is declared using `const`.
+// Variables declared with `const` cannot be reassigned or re-declared.
+
+// `age = age + 1` attempts to change the value of `age`,
+// which is not allowed when using `const`s.
+
+// To fix this problem, we should use `let` instead of `const`,
+// because `let` allows the value of a variable to be updated.
+
+let age = 33;
+age = age + 1; // age is now 34
+console.log(age);
+// Now the code works without errors.
