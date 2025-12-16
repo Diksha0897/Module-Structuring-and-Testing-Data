0a381e3c129bbe3a012b3d17a70f9bbe1e6b1bef
Diksha Aggarwal <dikshaaggarwal.in08@gmail.com>
2025-12-16 21:20:19 +0100

    Reorganize code comments and console.log to clarify variable declaration order

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,15 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+//console.log(`I was born in ${cityOfBirth}`);
+//const cityOfBirth = "Bolton";
+
+// The error happens because `cityOfBirth` is used before it is created.
+// JavaScript runs code from top to bottom.
+
+// Since `cityOfBirth` is declared after the console.log,
+// JavaScript does not know what `cityOfBirth` is yet.
+
+// Answer: We have to create the variable first, then use it.
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
