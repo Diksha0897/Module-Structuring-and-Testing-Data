faa3bf0791172d91c014d50ab9fa5d53f570eb60
Diksha Aggarwal <dikshaaggarwal.in08@gmail.com>
2025-12-20 00:30:59 +0100

    Created reusable toPounds function; handles numbers and strings like "399p" and prints in £x.xx format

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,30 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+// Function to convert pence into pounds and pence
+function toPounds(penceInput) {
+    // Convert input to string (in case it's a number)
+    let penceString = penceInput.toString();
+
+    // Remove the trailing "p" if it exists
+    penceString = penceString.replace("p", "");
+
+    // Make sure the string has at least 3 digits
+    const paddedPence = penceString.padStart(3, "0");
+
+    // Pounds = everything except the last two digits
+    const pounds = paddedPence.slice(0, -2);
+
+    // Pence = last two digits
+    const pence = paddedPence.slice(-2);
+
+    // Prints in £x.xx format
+    console.log(`£${pounds}.${pence}`);
+}
+
+//  Test the function
+toPounds(173);       // £1.73
+toPounds("11p");     // £0.11
+toPounds("4000p");   // £40.00
+toPounds("399p");    // £3.99
