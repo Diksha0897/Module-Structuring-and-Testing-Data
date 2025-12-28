0c42330c365bc745630080d824161eb7d8ca4533
Diksha Aggarwal <dikshaaggarwal.in08@gmail.com>
2025-12-28 16:55:41 +0100

    Solution for count .js using for loop

--- Sprint-3/3-mandatory-practice/implement/count.test.js
+++ Sprint-3/3-mandatory-practice/implement/count.test.js
@@ -10,11 +10,14 @@ const countChar = require("./count");
 // When the function is called with these inputs,
 // Then it should correctly count overlapping occurrences of char (e.g., 'a' appears five times in 'aaaaa').
 
+console.log(countChar("javascriptiscool", "i"));
+console.log(countChar("Dikshaislearning", "o"));
+
 test("should count multiple occurrences of a character", () => {
-  const str = "aaaaa";
-  const char = "a";
+  const str = "abbblluurrthulp";
+  const char = "u";
   const count = countChar(str, char);
-  expect(count).toEqual(5);
+  expect(count).toEqual(3);
 });
 
 // Scenario: No Occurrences
@@ -22,3 +25,10 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+test("should count 0 occurrences of a character", () => {
+  const str = "Dikshaislearning";
+  const char = "o";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
